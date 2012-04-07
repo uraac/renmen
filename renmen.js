b4eb1533f6cdef2fd0b04a50d1c5f16ed0e23806
uraac <nomail@has.com>
2012-04-07 18:53:27 +0900

    hoge

--- renmen.js
+++ renmen.js
@@ -1,3 +1,10 @@
+// javascript:
 (function(global){
+    if(!jQuery){
+        // load
+        var stag = document.createElement("script");
+        stag.scr = "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
+        global.document.appendChild(stag);
+    }
 
 })(this);
