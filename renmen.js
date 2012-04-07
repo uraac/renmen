26d5c3cc4f87a3b937fc4ec39eac45737ca2e81c
uraac <nomail@has.com>
2012-04-07 19:25:11 +0900

    load test

--- renmen.js
+++ renmen.js
@@ -1,10 +1,18 @@
 // javascript:
 (function(global){
-    if(!typeof jQuery){
-        // load
-        var stag = document.createElement("script");
-        stag.scr = "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
-        global.document.appendChild(stag);
+    var displayToolArea = function(){
+        global.alert("hoge");
     }
+    if(typeof jQuery === "undefined"){
+        // load
+        var stag = global.document.createElement("script");
+        stag.type = "text/javascript";
+        stag.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
+        global.document.body.appendChild(stag);
+        stag.onload = displayToolArea;
+    } else {
+        displayToolArea();
+    }
+
 
 })(this);
